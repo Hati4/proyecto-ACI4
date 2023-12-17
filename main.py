a43c8158b714659e99e43908c27b57f086e54b79
hati4 <jose_17_gajardo@hotmail.com>
2023-12-17 16:49:19 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""This ACI Sandbox Lab contains a shared ACI Server Instance.
+
+url: https://sandboxapicdc.cisco.com
+
+    username: admin
+    password: !v3G@!4@Y
+"""
+
+import request
+import json
+import conf
+
+print(conf.usuario)
+print(conf.clave)
